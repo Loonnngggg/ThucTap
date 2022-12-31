28e9a3f3b1f1cce34596c8a5ea5c7b491887eb54
Long <loonnngggg122333@gmail.com>
2022-12-31 09:32:19 +0700

    Cập nhật ghi chú

--- ThucTap.py
+++ ThucTap.py
@@ -18,15 +18,15 @@ class LinuxRouter(Node):
 
 class NetworkTopo(Topo):
     def build(self, **_opts):
-        # Add 2 routers in two different subnets
+        # Thêm 2 router ở 2 mạng khác nhau
         r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
         r2 = self.addHost('r2', cls=LinuxRouter, ip='10.1.0.1/24')
 
-        # Add 2 switches
+        # Thêm 2 switch
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
 
-        # Add host-switch links in the same subnet
+        # Thêm link giữa router-switch
         self.addLink(s1,
                      r1,
                      intfName2='r1-eth1',
@@ -37,7 +37,7 @@ class NetworkTopo(Topo):
                      intfName2='r2-eth1',
                      params2={'ip': '10.1.0.1/24'})
 
-        # Add router-router link in a new subnet for the router-router connection
+        # Thêm link giữa 2 router
         self.addLink(r1,
                      r2,
                      intfName1='r1-eth2',
@@ -45,7 +45,7 @@ class NetworkTopo(Topo):
                      params1={'ip': '10.100.0.1/24'},
                      params2={'ip': '10.100.0.2/24'})
 
-        # Adding hosts specifying the default route
+        # Thêm host
         h1 = self.addHost(name='h1',
                           ip='10.0.0.251/24',
                           defaultRoute='via 10.0.0.1')
@@ -59,7 +59,7 @@ class NetworkTopo(Topo):
                           ip='10.1.0.254/24',
                           defaultRoute='via 10.1.0.1')
 
-        # Add host-switch links
+        # Thêm link host-switch
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(h3, s2)
@@ -70,7 +70,7 @@ def run():
     topo = NetworkTopo()
     net = Mininet(topo=topo)
 
-    # Add routing for reaching networks that aren't directly connected
+    # Thêm định tuyến giữa 2 mạng
     info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev r1-eth2"))
     info(net['r2'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r2-eth2"))
 
