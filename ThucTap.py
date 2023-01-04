5a67ad23f70e8c408020eaf329170fea8226b9ce
Long <loonnngggg122333@gmail.com>
2023-01-04 16:43:46 +0700

    Đọc dữ liệu từ file input.txt

--- ThucTap.py
+++ ThucTap.py
@@ -5,6 +5,25 @@ from mininet.node import Node
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 
+# Đọc file input
+with open('input.txt', 'r') as f:
+    input = f.readlines()
+data = []
+for i in input:
+    data.append(i.strip())
+
+# Gắn địa chỉ cho các biến
+ipnetwork1 = data[2]    # Địa chỉ ip mạng 1
+ipnetwork2 = data[3]    # Địa chỉ ip mạng 2
+ipr1_in = data[5]       # Địa chỉ ip router 1 trong mạng 1
+ipr2_in = data[6]       # Địa chỉ ip router 2 trong mạng 2
+ipr1_out = data[8]      # Địa chỉ ip router 1 ngoài mạng
+ipr2_out = data[10]     # Địa chỉ ip router 2 ngoài mạng
+iph1 = data[11]         # Địa chỉ ip host 1
+iph2 = data[12]         # Địa chỉ ip host 2
+iph3 = data[13]         # Địa chỉ ip host 3
+iph4 = data[14]         # Địa chỉ ip host 4
+
 
 class LinuxRouter(Node):
     def config(self, **params):
@@ -19,8 +38,8 @@ class LinuxRouter(Node):
 class NetworkTopo(Topo):
     def build(self, **_opts):
         # Thêm 2 router ở 2 mạng khác nhau
-        r1 = self.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
-        r2 = self.addHost('r2', cls=LinuxRouter, ip='10.1.0.1/24')
+        r1 = self.addHost('r1', cls=LinuxRouter, ip=ipr1_in)
+        r2 = self.addHost('r2', cls=LinuxRouter, ip=ipr2_in)
 
         # Thêm 2 switch
         s1 = self.addSwitch('s1')
@@ -30,34 +49,34 @@ class NetworkTopo(Topo):
         self.addLink(s1,
                      r1,
                      intfName2='r1-eth1',
-                     params2={'ip': '10.0.0.1/24'})
+                     params2={'ip': ipr1_in})
 
         self.addLink(s2,
                      r2,
                      intfName2='r2-eth1',
-                     params2={'ip': '10.1.0.1/24'})
+                     params2={'ip': ipr2_in})
 
         # Thêm link giữa 2 router
         self.addLink(r1,
                      r2,
                      intfName1='r1-eth2',
                      intfName2='r2-eth2',
-                     params1={'ip': '10.100.0.1/24'},
-                     params2={'ip': '10.100.0.2/24'})
+                     params1={'ip': ipr1_out},
+                     params2={'ip': ipr2_out})
 
         # Thêm host
         h1 = self.addHost(name='h1',
-                          ip='10.0.0.251/24',
-                          defaultRoute='via 10.0.0.1')
+                          ip=iph1,
+                          defaultRoute='via ' + ipr1_in[:-3])
         h2 = self.addHost(name='h2',
-                          ip='10.0.0.252/24',
-                          defaultRoute='via 10.0.0.1')
+                          ip=iph2,
+                          defaultRoute='via ' + ipr1_in[:-3])
         h3 = self.addHost(name='h3',
-                          ip='10.1.0.253/24',
-                          defaultRoute='via 10.1.0.1')
+                          ip=iph3,
+                          defaultRoute='via ' + ipr2_in[:-3])
         h4 = self.addHost(name='h4',
-                          ip='10.1.0.254/24',
-                          defaultRoute='via 10.1.0.1')
+                          ip=iph4,
+                          defaultRoute='via ' + ipr2_in[:-3])
 
         # Thêm link host-switch
         self.addLink(h1, s1)
@@ -71,8 +90,12 @@ def run():
     net = Mininet(topo=topo)
 
     # Thêm định tuyến giữa 2 mạng
-    info(net['r1'].cmd("ip route add 10.1.0.0/24 via 10.100.0.2 dev r1-eth2"))
-    info(net['r2'].cmd("ip route add 10.0.0.0/24 via 10.100.0.1 dev r2-eth2"))
+    info(net['r1'].cmd("ip route add " + ipnetwork2 + " via " + ipr2_out[:-3] + " dev r1-eth2"))
+    # r1 cần đi đến mạng 2
+    # ipnetwork2 là địa chỉ mạng cần đến, ipr2_out là địa chỉ IP next hop, r1-eth2 là exit interface
+    info(net['r2'].cmd("ip route add " + ipnetwork1 + " via " + ipr1_out[:-3] + " dev r2-eth2"))
+    # r2 cần đi đến mạng 1
+    # ipnetwork1 là địa chỉ mạng cần đến, ipr1_out là địa chỉ IP next hop, r2-eth2 là exit interface
 
     net.start()
     CLI(net)
